c60e23dd2bf1633d7919202371bd0cd9913544cd
Adam Bilyea <adam.bilyea@uoit.net>
2019-05-14 18:03:25 -0400

    Added Camera Lag
    Im sick wit it

--- Horror2D/Intermediate/Build/Win64/Horror2DEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Horror2D/Intermediate/Build/Win64/Horror2DEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/eashv/Documents/GitKraken/2D-Horror-Game/Horror2D/Intermediate/Build/Win64/Horror2DEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/AdamBilyea/Documents/Unity Stuff/2D-Horror-Game/Horror2D/Intermediate/Build/Win64/Horror2DEditor/Development/Engine/SharedPCH.Engine.h"
